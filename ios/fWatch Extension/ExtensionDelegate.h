4eb293fb4794c8df71799e5dfcbc3ea3d6787bae
Eugene Lin <eugenelin89@gmail.com>
2015-09-25 20:27:20 -0700

    Add Watch Extension

---
+++ ios/fWatch Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  fWatch Extension
+//
+//  Created by Eugene Lin on 2015-09-25.
+//  Copyright © 2015 S5 Software Technologies Inc. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ ios/fWatch Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  fWatch Extension
+//
+//  Created by Eugene Lin on 2015-09-25.
+//  Copyright © 2015 S5 Software Technologies Inc. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
